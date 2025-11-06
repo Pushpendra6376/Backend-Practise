7b284c66150e5160d8fe4a57d0a705f5fffa1eaf
pushpendra6376 <pp5395021@gmail.com>
2025-11-06 20:57:08 +0530

    code clean up understood

--- Kickstart with node js/Reading-Writing/Task-2/app.js
+++ Kickstart with node js/Reading-Writing/Task-2/app.js
@@ -1,53 +1,7 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes')
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-
-    if (url === "/") {
-        // Step 1: Read file value and show above form
-        fs.readFile("formValue.txt", (err, data) => {
-            const savedValue = data ? data.toString() : "";
-
-            res.setHeader("Content-Type", "text/html");
-            res.end(`
-                <h1>Saved Value: ${savedValue}</h1>
-                <form action="/message" method="POST">
-                    <label>Name:</label>
-                    <input type="text" name="username" />
-                    <button type="submit">Add</button>
-                </form>
-            `);
-        });
-    }
-
-    else if (url === "/message" && method === "POST") {
-        let dataChunks = [];
-
-        req.on("data", (chunk) => {
-            dataChunks.push(chunk);
-        });
-
-        req.on("end", () => {
-            let buffer = Buffer.concat(dataChunks);
-            let formData = buffer.toString();     // username=Pushpendra
-            let formValue = formData.split("=")[1];
-
-            // Step 2: Save entered value to file
-            fs.writeFile("formValue.txt", formValue, () => {
-                res.statusCode = 302;
-                res.setHeader("Location", "/");   // Redirect back to home
-                res.end();
-            });
-        });
-    }
-
-    else {
-        res.statusCode = 404;
-        res.end("Page Not Found");
-    }
-});
+const server = http.createServer(routes);
 
 server.listen(3000, () => {
     console.log("server is running");
