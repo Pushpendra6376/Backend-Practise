2cb2a73c480f57a8f22d3a31cf1c4ec3e7278eb4
pushpendra6376 <pp5395021@gmail.com>
2025-11-06 22:23:21 +0530

    devdependecies understood

---
+++ Kickstart with node js/packagesInstallation/index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer((req, res) =>{
+
+    if(req.url == '/'){
+        res.end(`
+            <h1>hello</h1>
+            <h1>This is the home page</h1>`)
+    }
+})
+
+server.listen(3000, ()=>{
+    console.log("server is running")
+})
