118b3957ed3af8963fafa7c5e8f8850060aab944
Pushpendra Patel <134173726+Pushpendra6376@users.noreply.github.com>
2025-11-13 19:35:53 +0530

    Delete an-E-commerce-StartUp directory

--- an-E-commerce-StartUp/routes/cart.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cartController = require('../controllers/cartController')
-
-router.get('/:userId', cartController.getCartById);
-
-router.post('/:userId', cartController.postCartById);
-
-module.exports = router;
